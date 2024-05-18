d5f67e821f0cba2fa34bf591b749592028fca8ed
jessica-slee4275 <50062122+jessica-slee4275@users.noreply.github.com>
2024-05-18 14:35:00 -0400

    Create SolveMeFirst.py

---
+++ WarmUp/Easy/SolveMeFirst.py
@@ -0,0 +1,20 @@
+"""
+
+input
+a = 2
+b = 3
+
+output
+5
+"""
+
+
+
+def solveMeFirst(a,b):
+	# Hint: Type return a+b below
+    return a+b
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
